b462e4e83b10a7e515aaa7e0f57c7bdeaac911ef
Martin Grotz <martin.grotz@mathema.de>
2022-02-02 22:54:35 +0100

    add DragonShield format parse function

--- lib/missing_cards.ex
+++ lib/missing_cards.ex
@@ -1,7 +1,20 @@
 defmodule MissingCards do
 
-  def parseOneLine(line) do
+  def parseOneMtgoLine(line) do
     [count | name_parts] = String.split(line)
     name = Enum.join(name_parts, " ")
+
+    {String.to_integer(count), name}
+  end
+
+  def parseOneHavesLine(line) do
+    [count | rest] = String.split(line, ",")
+
+    name =
+      Enum.slice(rest, 0..-7)
+      |> Enum.join(",")
+      |> String.trim()
+
+    {String.to_integer(count), name}
   end
 end
--- test/missing_cards_test.exs
+++ test/missing_cards_test.exs
@@ -1,9 +1,21 @@
 defmodule MissingCardsTest do
   use ExUnit.Case
 
-  @haves "1, Battlefield Raptor, M20, NearMint, 0.03, Normal, German, 2021-11-27"
+  test "MTGO format line gets parsed into number and name" do
+    assert (MissingCards.parseOneMtgoLine("4 Battlefield Raptor") == {4, "Battlefield Raptor"})
+  end
 
-  test "greets the world" do
-    assert (MissingCards.parseOneLine("4 Battlefield Raptor") == {})
+  test "Another MTGO format line gets parsed into number and name" do
+    assert (MissingCards.parseOneMtgoLine("2 Green Turtle") == {2, "Green Turtle"})
+  end
+
+  test "DragonShield CardScanner format line gets parsed into number and name" do
+    actual = MissingCards.parseOneHavesLine("1, Battlefield Raptor, M20, NearMint, 0.03, Normal, German, 2021-11-27")
+    assert (actual == {1, "Battlefield Raptor"})
+  end
+
+  test "DragonShield CardScanner format line gets parsed into number and name even when name contains a comma" do
+    actual = MissingCards.parseOneHavesLine("1, Teferi, Master of Time, M21, NearMint, 8.46, Normal, German, 2022-02-02")
+    assert (actual == {1, "Teferi, Master of Time"})
   end
 end
