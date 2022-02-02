da375fe88898238b40ea8213ee1d9e6e12c1432d
Martin Grotz <martin.grotz@mathema.de>
2022-02-02 21:56:33 +0100

    start of project

---
+++ lib/missing_cards.ex
@@ -0,0 +1,18 @@
+defmodule MissingCards do
+  @moduledoc """
+  Documentation for `MissingCards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MissingCards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/missing_cards_test.exs
@@ -0,0 +1,8 @@
+defmodule MissingCardsTest do
+  use ExUnit.Case
+  doctest MissingCards
+
+  test "greets the world" do
+    assert MissingCards.hello() == :world
+  end
+end
